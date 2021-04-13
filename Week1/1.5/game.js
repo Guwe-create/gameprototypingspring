273789998f9b13e11cab58703b7073261123b580
Guwe-create <whroy@email.neit.edu>
2021-04-13 17:42:21 -0400

    first commit

---
+++ Week1/1.5/game.js
@@ -0,0 +1,74 @@
+
+
+var canvas;
+var context;
+var timer;
+
+var mainTimer = setInterval(animate, 1000/60);
+var ball;
+var speed;
+speed = 1;
+
+var up;
+up = 1;
+
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	ball = new GameObject();
+	ball.x = 20;
+
+function animate()
+{
+	
+	
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	
+	if (up == 1)
+	{
+		ball.y = ball.y + 1;
+	}
+	if (ball.y == 760)
+	{
+		up = 2;
+		ball.color = "green";
+	}
+	if (speed == 1)
+	{
+		ball.x = ball.x + 1;
+	}
+	if (up == 2)
+	{
+		ball.y = ball.y - 1;
+	}
+	if (ball.y == 30)
+	{
+		up = 1;
+		ball.color = "blue";
+	}
+	if (ball.x == 980)
+	{
+		speed = 2;
+		ball.color = "yellow";
+		
+	}
+	if (speed == 2)
+	{
+		ball.x = ball.x - 1;
+	}
+	if (ball.x == 30)
+	{
+		speed = 1;
+		ball.color = "red";
+		
+	}
+
+	
+		
+	
+	ball.drawCircle();
+	
+}
+
